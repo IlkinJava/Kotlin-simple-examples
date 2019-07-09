cb53e6109cc85e6080b6c5b75af85a92fbd238c2
IlkinJava <ialiyevv878@gmail.com>
2019-07-09 17:12:35 +0400

    *

--- src/collections/Main.kt
+++ src/collections/Main.kt
@@ -1,10 +1,12 @@
 package collections
 
 fun main(args: Array<String>) {
-    var list = listOf(1,2,3)
-    var set = setOf(1,2,2,3)
-    var map = mapOf(1 to "one",2 to "two")
+    val list = listOf(1,2,3)
+    val set = setOf(1,2,2,3)
+    val map = mapOf(1 to "one",2 to "two")
 
     println(list.max())
     println(list.average())
+    println(set)
+    println(map)
 }
